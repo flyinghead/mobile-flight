b95e636f2b8139e86a814e546d151414cab7fad7
Flyinghead <raphael.jean@gmail.com>
2017-08-01 18:54:55 +0200

    Added custom airplane, custom tri and quad 1234 mixer types

--- MobileFlight/MultiTypes.swift
+++ MobileFlight/MultiTypes.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class MultiTypes {
-    static let label = [ "Tricopter", "Quad +", "Quad X", "Bicopter", "Gimbal", "Y6", "Hex +", "Flying Wing", "Y4", "Hex X", "Octo X8", "Octo Flat +", "Octo Flat X", "Airplane", "Heli 120", "Heli 90", "V-tail Quad", "Hex H", "PPM to SERVO", "Dualcopter", "Singlecopter", "A-tail Quad", "Custom"]
+    static let label = [ "Tricopter", "Quad +", "Quad X", "Bicopter", "Gimbal", "Y6", "Hex +", "Flying Wing", "Y4", "Hex X", "Octo X8", "Octo Flat +", "Octo Flat X", "Airplane", "Heli 120", "Heli 90", "V-tail Quad", "Hex H", "PPM to SERVO", "Dualcopter", "Singlecopter", "A-tail Quad", "Custom", "Custom Airplane", "Custom Tricopter", "Quad X 1234" ]
     static let custom = "custom"
-    static let drawings = [ "tri", "quad_p", "quad_x", "bicopter", custom, "y6", "hex_p", "flying_wing", "y4", "hex_x", "octo_x8", "octo_flat_p", "octo_flat_x", "airplane", custom, custom, "vtail_quad", custom, custom, custom, custom, "atail_quad", custom]
+    static let drawings = [ "tri", "quad_p", "quad_x", "bicopter", custom, "y6", "hex_p", "flying_wing", "y4", "hex_x", "octo_x8", "octo_flat_p", "octo_flat_x", "airplane", custom, custom, "vtail_quad", custom, custom, custom, custom, "atail_quad", custom, custom, custom, "quad_x_1234" ]
     
     static func getDescription(multiType: Int) -> String {
         if multiType < 1 || multiType > label.count {
